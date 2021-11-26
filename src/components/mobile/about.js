195245d04af459d6c9f991462d92616d5378a8a5
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-11-27 00:43:24 +0800

    add mobile about page

---
+++ src/components/mobile/about.js
@@ -0,0 +1,21 @@
+import styled   from 'styled-components'
+
+const Div = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: 10vw
+`
+
+
+
+function About() {
+    return (
+        <Div>About Page</Div>
+    )
+}
+
+
+export default About;
--- src/components/mobile/container.js
+++ src/components/mobile/container.js
@@ -1,21 +1,13 @@
-import styled  from 'styled-components'
 import { Routes ,Route } from 'react-router-dom';
 import Main from '../mobile/main'
-
-const Div = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-`
+import About from './about'
     
 
 function Mobile() {
     return (
         <Routes>
             <Route exact path="/" element={<Main />} />
-            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/about" element={<About />} />
             <Route element={<Main />} />
         </Routes>
     )
